5b8b6be60c9f2ca86aa2596659929c9da561662d
Will Macrae <wmacrae@palantir.com>
2020-09-03 11:04:53 -0700

    Updates

---
+++ precache-manifest.5d8a659b588e4a51db355600bb796593.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2c84c788ed6ae7864d6ed4fb16471c77",
+    "url": "/party-calc/index.html"
+  },
+  {
+    "revision": "7236bcb4d8088cb06217",
+    "url": "/party-calc/static/css/main.c5ba3190.chunk.css"
+  },
+  {
+    "revision": "d34c47864c027a79b126",
+    "url": "/party-calc/static/js/2.38d8089b.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/party-calc/static/js/2.38d8089b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7236bcb4d8088cb06217",
+    "url": "/party-calc/static/js/main.e487b17d.chunk.js"
+  },
+  {
+    "revision": "9258aee5fcc79390ed81",
+    "url": "/party-calc/static/js/runtime-main.80518510.js"
+  }
+]);
